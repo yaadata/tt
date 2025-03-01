29e71af3813039ebf23625ddeaa2744456a9b4ce
Yadi Abdalhalim <36752216+Ydot19@users.noreply.github.com>
2025-03-01 10:11:56 -0800

    Chore: Add GitHub actions (#1)
    * chore: remove duplicate test
    
    * ci: add lib tests ci/cd step
    
    * fix: quiet flag
    
    * fix ci attempt #3
    
    * fix ci attempt #4
    
    * fix ci attempt #5

---
+++ .github/pull_request_template.md
@@ -0,0 +1,26 @@
+## Related Issue  
+<!-- Attached the related issue -->
+
+### Additional Context
+
+<!-- (Optional) add additional context about pull-request about. -->
+
+### How is it implemented
+
+<!-- How is this change implemented -->
+
+### Check the related fields
+
+- [ ] Documentation <!-- Updates the documentation and related to this repository -->
+- [ ] Feature <!-- Adds a net new capability -->
+- [ ] Enhancement <!-- updates an existing feature and adds to it -->
+- [ ] Test <!-- updates or adds new tests -->
+- [ ] Chore
+<!--
+updates the way this repository is run
+- ci
+- issue templates
+- license
+- deployment
+- release
+-->
--- lib/framework/golang/gotest_test.rs
+++ lib/framework/golang/gotest_test.rs
@@ -24,7 +24,7 @@ mod test {
         "#;
         let buffer = Buffer::new(
             content,
-            "src/fixtures/golang/sample_test.go".to_string(),
+            "sample_test.go".to_string(),
             types::CursorPosition::new(10, 3),
         );
         let target = Target::new(crate::core::enums::ToolCategory::TestRunner, buffer);
@@ -39,51 +39,6 @@ mod test {
         let res = gotest::get_parent_test(Some(walker.node()), &target);
         // assert
         assert!(res.is_none());
-    }
-
-    #[gtest]
-    fn get_test_function_name() {
-        // arrange
-        let content = r#"
-        package golang
-        import (
-          l "testing"
-
-          "github.com/stretchr/testify/assert"
-        )
-
-        func sample_add(a, b int) int {
-          return a + b
-        }
-
-        func TestSample(t *testing.T) {
-            t.Run("case_a", func(t *testing.T){
-              assert.Equal(t, 1, sample_add(1, 0))
-            })
-            t.Run("case_b", func(t *testing.T){
-              assert.Equal(t, 2, sample_add(1, 2))
-            })
-        }
-        "#;
-        let buffer = Buffer::new(
-            content,
-            "src/fixtures/golang/base_test.go".to_string(),
-            types::CursorPosition::new(16, 3),
-        );
-        let target = Target::new(enums::ToolCategory::TestRunner, buffer);
-        let tree = common::utils::parse_tree(content);
-        assert_that!(tree, ok(anything()));
-        let tree = tree.unwrap();
-        let mut walker = tree.walk();
-
-        let cursor = types::CursorPosition::new(16, 3);
-        walker.goto_first_child_for_point(cursor.to_point());
-
-        // act
-        let res = gotest::get_parent_test(Some(walker.node()), &target);
-        // assert
-        assert!(res.is_some());
-        assert_eq!(res.unwrap().name, "TestSample");
     }
 
     #[gtest]
@@ -116,10 +71,6 @@ mod test {
             t.Run("case_b", func(t *testing.T){
               assert.Equal(t, 2, sample_add(1, 2))
             })
-        }
-
-        func TestSecond(t *testing.T) {
-            assert.Equal(t, 2, sample_add(1, 2))
         }
         "#;
 
