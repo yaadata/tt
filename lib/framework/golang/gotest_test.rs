22e1b0399d5fe1e270005ff128b98d75749e1daa
Yadi Abdalhalim <abdalhalim.yaadata@gmail.com>
2025-03-08 17:39:37 -0600

    test: add test for get in loop typed subcase with naemd case fields

--- lib/framework/golang/gotest_test.rs
+++ lib/framework/golang/gotest_test.rs
@@ -419,4 +419,96 @@ mod test {
             assert_that!(runnable.is_some(), eq(true));
         }
     }
+
+    #[gtest]
+    #[rstest]
+    #[case(enums::Search::InFile, types::CursorPosition::new(19, 3), 2, vec!["TestGetInLoopTypedSubcaseWithNamedCaseFields/base case", "TestGetInLoopTypedSubcaseWithNamedCaseFields/case 1"])]
+    #[case(enums::Search::Nearest, types::CursorPosition::new(21, 3), 1, vec!["TestGetInLoopTypedSubcaseWithNamedCaseFields/base case"])]
+    #[case(enums::Search::Nearest, types::CursorPosition::new(22, 3), 1, vec!["TestGetInLoopTypedSubcaseWithNamedCaseFields/base case"])]
+    #[case(enums::Search::Nearest, types::CursorPosition::new(26, 3), 1, vec!["TestGetInLoopTypedSubcaseWithNamedCaseFields/base case"])]
+    #[case(enums::Search::Nearest, types::CursorPosition::new(27, 3), 1, vec!["TestGetInLoopTypedSubcaseWithNamedCaseFields/case 1"])]
+    #[case(enums::Search::Nearest, types::CursorPosition::new(32, 3), 1, vec!["TestGetInLoopTypedSubcaseWithNamedCaseFields/case 1"])]
+    fn get_in_loop_typed_subcase_with_named_case_fields(
+        #[case] search: enums::Search,
+        #[case] position: types::CursorPosition,
+        #[case] expected_num_of_tests: usize,
+        #[case] expected_test_names: Vec<&str>,
+    ) {
+        // ARRANGE
+        let content = r#"
+        package golang
+        import (
+          "testing"
+
+          "github.com/stretchr/testify/assert"
+        )
+
+        func sample_add(a, b int) int {
+          return a + b
+        }
+
+        func TestGetInLoopTypedSubcaseWithNamedCaseFields(t *testing.T) {
+          type Scenario struct {
+              description string
+                a			int
+                b			int
+                expected	int
+            }
+
+          for _, tt := range []Scenario{
+            {
+              description: "base case",
+              a:          0,
+              b:          3,
+              c:          3,
+            },
+            {
+              description: "case 1",
+              a:           1,
+              b:           3,
+              expected:    4,
+            },
+          }  {
+            t.Run(tt.description, func(t *testing.T) {
+              actual := sample_add(tt.a, tt.b)
+              assert.Equal(t, tt.expected, actual)
+            })
+          }
+        }
+        "#;
+
+        let buffer = Buffer::new(content, "run_test.go".to_string(), position);
+        let mut target = Target::new(enums::ToolCategory::TestRunner, buffer);
+        target.override_search_strategy(search);
+
+        let tree = common::utils::parse_tree(content);
+        assert_that!(tree, ok(anything()));
+        let tree = tree.unwrap();
+        let mut walker = tree.walk();
+
+        walker.goto_first_child_for_point(position.to_point());
+
+        let node = walker.node();
+        let parent_runnable = get_parent_test(Some(node), &target);
+        assert_that!(parent_runnable.is_some(), eq(true));
+        let parent_runnable = parent_runnable.unwrap();
+        assert_eq!(
+            parent_runnable.name,
+            "TestGetInLoopTypedSubcaseWithNamedCaseFields"
+        );
+
+        walker.reset(node);
+
+        // ACT
+        let res = get_sub_tests(Some(node), Some(parent_runnable), &target);
+
+        // ASSERT
+        assert_that!(res.is_some(), eq(true));
+        let res = res.unwrap();
+        assert_that!(res.len(), eq(expected_num_of_tests));
+        for ts in expected_test_names {
+            let runnable = res.iter().find(|&x| x.name == ts);
+            assert_that!(runnable.is_some(), eq(true));
+        }
+    }
 }
