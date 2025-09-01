d3d5846c67f1807720c9eb589c9613a56dd5bd66
Yadi Abdalhalim <abdalhalim.yaadata@gmail.com>
2025-09-01 01:27:52 -0500

    miss copy

--- lib/framework/golang/treesitter/queries/gotest_subtest_in_loop_unnamed_fields.rs
+++ lib/framework/golang/treesitter/queries/gotest_subtest_in_loop_unnamed_fields.rs
@@ -39,56 +39,64 @@
 pub(crate) fn query() -> String {
     let res = r#"
         [[
-          ((for_statement
-            (range_clause
-              left: (expression_list
-                (identifier)
-                (identifier) @test.loop.case.variable
-              )
-              right: (composite_literal
-                type: (slice_type
-                  element: (struct_type
-                    (field_declaration_list
-                      (field_declaration
-                      	name: (field_identifier) @test.case.definition.field
-                        type: (type_identifier) @test.case.definition.field.type (#eq? @test.case.definition.field.type "string")
-                      )
-                    )
-                  ) @test.case.type
-                )
-                body: (literal_value
-                  (literal_element
-                    (literal_value
-                      (keyed_element
-                        (literal_element
-                          (identifier)
-                        )  @test.case.field.name (#eq? @test.case.field.name @test.case.definition.field)
-                        (literal_element
-                          (interpreted_string_literal) @test.case.field.value
+              ;; query for function name
+              ((function_declaration 
+                                name: (identifier) @_test.parent.name
+                                parameters: (parameter_list
+                                    (parameter_declaration
+                                             name: (identifier) @_test.parent.var
+                                             type: (pointer_type
+                                                 (qualified_type
+                                                  package: (package_identifier) @_test.param_package
+                                                  name: (type_identifier) @_test.param_name))))
+                                 ) @testfunc
+                              (#contains? @_test.parent.name "Test"))
+              ;; query for list table tests (wrapped in loop)
+              (for_statement
+                (range_clause
+                  left: (expression_list
+                    (identifier)
+                    (identifier) @test.loop.case.variable
+                  )
+                  right: (composite_literal
+                    type: (slice_type
+                      element: (struct_type
+                        (field_declaration_list
+                          (field_declaration
+                            name: (field_identifier) @test.case.definition.field
+                            type: (type_identifier) @test.case.definition.field.type (#eq? @test.case.definition.field.type "string")
+                          )
                         )
                       )
-                    ) @test.case
+                    )
+                    body: (literal_value
+                      (literal_element
+                        (literal_value
+                          (literal_element
+                            (interpreted_string_literal) @test.case.field.value
+                          )
+                        ) 
+                      ) @test.case
+                    )
+                  )
+                )
+                body: (block
+                  (expression_statement
+                    (call_expression
+                      function: (selector_expression
+                        operand: (identifier) @test.loop.test
+                        field: (field_identifier) @test.loop.test.method (#eq? @test.loop.test.method "Run")
+                      )
+                      arguments: (argument_list
+                        (selector_expression
+                          operand: (identifier) @test.loop.test.variable (#eq? @test.loop.test.variable @test.loop.case.variable)
+                          field: (field_identifier) @test.loop.test.variable.field (#eq? @test.loop.test.variable.field @test.case.definition.field)
+                        )
+                      )
+                    )
                   )
                 )
               )
-            )
-            body: (block
-              (expression_statement
-                (call_expression
-                  function: (selector_expression
-                    operand: (identifier) @test.loop.test
-                    field: (field_identifier) @test.loop.test.method (#eq? @test.loop.test.method "Run")
-                  )
-                  arguments: (argument_list
-                    (selector_expression
-                      operand: (identifier) @test.loop.test.variable (#eq? @test.loop.test.variable @test.loop.case.variable)
-                      field: (field_identifier) @test.loop.test.variable.field (#eq? @test.loop.test.variable.field @test.case.definition.field)
-                    ) 
-                  )
-                )
-              )
-            )
-          ))
         ]]"#;
 
     res.to_string()
